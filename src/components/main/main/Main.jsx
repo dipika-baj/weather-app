3b151f76fede67afd661767da8037825b0cfe6c5
dipikabajracharya <dipika@fullstack-wp.com>
2024-01-30 21:07:28 +0545

    edited forcast type and unit in navbar

--- src/components/main/main/Main.jsx
+++ src/components/main/main/Main.jsx
@@ -4,6 +4,7 @@ import TodayWeather from "../../today'sWeather/TodayWeather";
 import style from "./main.module.css";
 const Main = (props) => {
     const { weeklyWeather, currentTimezone, currentWeather } = props;
+    console.log("main");
     return (
         <div className={style.weather_detail_wrapper}>
             <NavBar />
--- src/components/main/navbar/NavBar.jsx
+++ src/components/main/navbar/NavBar.jsx
@@ -1,31 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./navbar.module.css";
 import { SquareUserRound } from "lucide-react";
 
 const NavBar = () => {
-    const [activeForcast, setActiveForcast] = useState("today");
+    const [activeForcast, setActiveForcast] = useState("week");
+    const [activeUnit, setActiveUnit] = useState("c");
+
     return (
         <nav className={styles.nav_bar}>
             <div className={styles.nav_menu}>
                 <ul className={styles.forecast_selector}>
-                    <li>
+                    <li
+                        className={
+                            activeForcast === "today" ? styles.active : ""
+                        }
+                        onClick={() => setActiveForcast("today")}
+                    >
                         <span>Today</span>
                     </li>
-                    <li className={styles.active}>
+                    <li
+                        className={
+                            activeForcast === "week" ? styles.active : ""
+                        }
+                        onClick={() => setActiveForcast("week")}
+                    >
                         <span>Week</span>
                     </li>
                 </ul>
                 <ul className={styles.unit_selector}>
-                    <li className={styles.active}>
+                    <li
+                        className={activeUnit === "c" ? styles.active : ""}
+                        onClick={() => setActiveUnit("c")}
+                    >
                         <span>&deg;C</span>
                     </li>
-                    <li>
+                    <li
+                        className={activeUnit === "f" ? styles.active : ""}
+                        onClick={() => setActiveUnit("f")}
+                    >
                         <span>&deg;F</span>
                     </li>
                 </ul>
             </div>
             <div className={styles.user_profile}>
-                {/* <img src="/user-profile.jpg" /> */}
                 <SquareUserRound />
             </div>
         </nav>
