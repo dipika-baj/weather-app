9d2938b05e2fae1b7047636484d7edde19d8dcd5
dipikabajracharya <dipikabajracharya57@gmail.com>
2024-02-17 19:26:41 +0545

    implemented debounce in search

--- src/components/sidebar/searchBar/SearchBar.jsx
+++ src/components/sidebar/searchBar/SearchBar.jsx
@@ -5,31 +5,41 @@ import { WeatherContext } from "../../../context/WeatherContextProvider";
 
 const SearchBar = () => {
     const [search, setSearch] = useState("");
-    const [searchData, setSearchData] = useState([]);
+    const [searchData, setSearchData] = useState();
     const [isLoading, setIsLoading] = useState(false);
+    const [showSearchResult, setShowSearchResult] = useState(false);
     const { setCoordinates } = useContext(WeatherContext);
+
+    console.log(search, searchData, isLoading);
 
     const changeSearch = (event) => {
         setSearch(event.target.value);
+        event.target.value
+            ? setShowSearchResult(true)
+            : setShowSearchResult(false);
     };
 
     useEffect(() => {
-        if (search) {
-            const api = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${
-                import.meta.env.VITE_APP_ID
-            }`;
-            setIsLoading(true);
-            fetch(api)
-                .then((response) => response.json())
-                .then((data) => {
-                    setSearchData(data);
-                })
-                .catch((err) => {
-                    console.log("Error:", err);
-                })
-                .finally(() => setIsLoading(false));
+        let trimmedSearch = search.trim();
+        if (trimmedSearch) {
+            const timer = setTimeout(() => {
+                const api = `https://api.openweathermap.org/geo/1.0/direct?q=${trimmedSearch}&limit=5&appid=${
+                    import.meta.env.VITE_APP_ID
+                }`;
+                if (!searchData) setIsLoading(true);
+                fetch(api)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        setSearchData(data);
+                    })
+                    .catch((err) => {
+                        console.log("Error:", err);
+                    })
+                    .finally(() => setIsLoading(false));
+            }, 500);
+            return () => clearTimeout(timer);
         } else {
-            setSearchData([]);
+            setSearchData();
         }
     }, [search]);
 
@@ -40,6 +50,7 @@ const SearchBar = () => {
                     <Search />
                 </label>
                 <input
+                    autoComplete="off"
                     type="text"
                     id="search"
                     placeholder="Search for places..."
@@ -47,10 +58,11 @@ const SearchBar = () => {
                     onChange={changeSearch}
                 />
             </div>
-            {!!search && (
+            {showSearchResult && (
                 <ul className={styles.search_result}>
                     <SearchResult
                         searchData={searchData}
+                        setShowSearchResult={setShowSearchResult}
                         isLoading={isLoading}
                         setCoordinates={setCoordinates}
                     />
@@ -62,18 +74,22 @@ const SearchBar = () => {
 export default SearchBar;
 
 const SearchResult = (props) => {
-    const { searchData, isLoading, setCoordinates } = props;
+    const { searchData, setShowSearchResult, isLoading, setCoordinates } =
+        props;
     if (isLoading) {
         return <li>Loading</li>;
     }
 
-    if (searchData.length === 0) {
+    if (searchData?.length === 0) {
         return <li>No city</li>;
     }
-    return searchData.map((data, index) => (
+    return searchData?.map((data, index) => (
         <li
             key={index}
-            onClick={() => setCoordinates({ lat: data.lat, lon: data.lon })}
+            onClick={() => {
+                setCoordinates({ lat: data.lat, lon: data.lon });
+                setShowSearchResult(false);
+            }}
         >
             {data.name} , {data.country}
         </li>
